6baa432f5e58350e4361eb30c7910d3de7205beb
nuts-rice <red44443@gmail.com>
2024-08-29 12:43:17 -0400

    🏷 model types for lobby privatelobby️

---
+++ src/app/model/privateLobby.ts
@@ -0,0 +1,8 @@
+import { Lobby } from './lobby';
+export interface PrivateLobby extends Lobby {
+  whiteList: string[];
+}
+
+export type PrivateLobbyAction =
+| { type: 'setWhiteList', payload: PrivateLobby["whiteList"] }
+
--- src/app/model/round.tsx
+++ src/app/model/round.tsx
@@ -8,11 +8,11 @@ export enum RoundStatus {
 
 export interface Round {
   id?: string;
-  selectedAccountHandle?: string;
   voterAddresses?: string[];
   totalBettingPool?: number;
   voterBets?: { [address: string]: Bet };
   roundStartTime?: number;
   roundEndTime?: number;
   status?: RoundStatus;
+  isPrivate?: boolean;
 }
