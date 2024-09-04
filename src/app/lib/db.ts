6849d53d2d34c105d0ab93551afe32cf92c697e2
nuts-rice <red44443@gmail.com>
2024-09-04 13:33:09 -0400

    🔒 secrets️

--- src/app/lib/db.ts
+++
@@ -1,8 +0,0 @@
-import { Redis } from "@upstash/redis";
-import "dotenv/config";
-const redis = new Redis({
-  url: "https://frank-bass-60714.upstash.io",
-  token: "Ae0qAAIjcDFkMmI1ZTU3YTY5MjU0ZmJlOTU0NjU5NTRjZGE4ZjE1OXAxMA",
-});
-
-export default redis;
